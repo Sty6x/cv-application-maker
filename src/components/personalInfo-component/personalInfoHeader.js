893bc424c55d47586816d2a87505e01d79589512
Fdiaz <jerylfranz460@gmail.com>
2023-01-04 10:07:47 +0400

    termporary styling

--- src/components/personalInfo-component/personalInfoHeader.js
+++ src/components/personalInfo-component/personalInfoHeader.js
@@ -1,17 +1,16 @@
 import React from "react";
 
-class PersonalInfoHeader extends React.Component{
-    constructor(props){
-        super(props)
-    }
+class PersonalInfoHeader extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-
-    render(){
-        return(
-            <div id="header-info-container">
-                <h1> Jeryl Franz Diaz Benjamin</h1>
-                <h2> Front End Software Engineer</h2>
-            </div>
-        )
-    }
+	render() {
+		return (
+			<div id="header-info-container">
+				<h1> Jeryl Franz Diaz Benjamin</h1>
+				<h2> Front End Software Engineer</h2>
+			</div>
+		);
+	}
 }
