4892670057d49b6eb6acdeb955d44f9fa3ce5193
Fdiaz <jerylfranz460@gmail.com>
2023-01-05 17:23:34 +0400

    idk

--- src/components/personalInfo-component/CVphoto.js
+++ src/components/personalInfo-component/CVphoto.js
@@ -11,7 +11,7 @@ class CVphoto extends React.Component {
 			<div id="cv-photo-container" className="flex flex-col items-center">
 				<div
 					id="photo-ph"
-					className="bg-[#fabca9] flex justify-center items-center border-2 border-black border-solid rounded-full w-[150px] h-[150px]"
+					className="bg-[#fabca9] flex justify-center items-center rounded-full w-[170px] h-[170px]"
 				>
 					<img className="rounded-full " src={img} id="cv-photo" alt="cv photo"></img>
 				</div>
