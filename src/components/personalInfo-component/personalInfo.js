6077a18424734ad9243ed08f90433aa25282322b
Fdiaz <jerylfranz460@gmail.com>
2023-01-05 17:37:52 +0400

    adjust side contents of cv

--- src/components/personalInfo-component/personalInfo.js
+++ src/components/personalInfo-component/personalInfo.js
@@ -9,7 +9,7 @@ class PersonalInfo extends React.Component{
     
     render(){
         return(
-            <div id="personal-info-container" className="text-white">
+            <div id="personal-info-container" className="flex flex-col text-white">
                 <h3 className="side-titles">Personal Information:</h3>
                 <div id="email-address">
                     <p>Jerylfranz@gmail.com</p>
