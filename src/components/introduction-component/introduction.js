e44f5a7f1194de092991bc83becf5fd05b8b94e9
Fdiaz <jerylfranz460@gmail.com>
2023-01-09 14:42:25 +0400

    able to append introduction input to display

--- src/components/introduction-component/introduction.js
+++ src/components/introduction-component/introduction.js
@@ -7,13 +7,10 @@ class Introduction extends React.Component {
 
 	render() {
 		return (
-			<div id="introduction-container" className="h-[210px]">
+			<div id="introduction-container" className="h-[210px] min-w-full">
 				<h3 className="main-titles">Introduction</h3>
-				<p className="indent-5">
-					Lorem6 ipsum odor amet, consectetuer adipiscing elit. Vitae hendrerit aenean euismod bibendum
-					fau1 00cibus aliquet cras risus nibh conubia rutrum elementum luctus; gravida ultricies
-					ullamcorper vest200ibulum potenti massa conubia. Dictum magna curabitur gravida nisi curae
-					nascetur ut feugiat volut300pat cubilia ultrices rhoncus hac gravida adipiscing habitant.
+				<p className="whitespace-normal indent-5">
+					{this.props.intro}
 				</p>
 			</div>
 		);
